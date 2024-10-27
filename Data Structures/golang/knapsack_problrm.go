920c53fa9413609c25b5cad54fda16df290e5c33
nikhilkumarjha62 <92928026+nikhilkumarjha62@users.noreply.github.com>
2024-10-27 13:54:01 +0530

    Create knapsack_problrm.go

---
+++ Data Structures/golang/knapsack_problrm.go
@@ -0,0 +1,18 @@
+func knapsack(weights, values []int, capacity int) int {
+    n := len(weights)
+    dp := make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, capacity+1)
+    }
+
+    for i := 1; i <= n; i++ {
+        for w := 1; w <= capacity; w++ {
+            if weights[i-1] <= w {
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]]+values[i-1])
+            } else {
+                dp[i][w] = dp[i-1][w]
+            }
+        }
+    }
+    return dp[n][capacity]
+}
